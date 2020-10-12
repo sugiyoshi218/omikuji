cadc3b2889b57831f11a6a172e674ebdde39b366
sugiyoshi218 <72730557+sugiyoshi218@users.noreply.github.com>
2020-10-12 17:15:44 +0900

    initial commit
    簡単なおみくじを作りました。

---
+++ js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const btn = document.getElementById("button");
+
+btn.addEventListener("click", () => {
+  const result = ["great", "good", "bad"];
+  const n = Math.floor(Math.random() * result.length);
+  btn.textContent=result[n];
+});
