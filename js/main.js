159d7e540a797b668331e737d029a63c262e7dbb
sugiyoshi218 <72730557+sugiyoshi218@users.noreply.github.com>
2020-10-25 14:14:19 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 "use strict";
 
-const btn = document.getElementById("button");
+(()=>{
 
-btn.addEventListener("click", () => {
-  const result = ["great", "good", "bad"];
-  const n = Math.floor(Math.random() * result.length);
-  btn.textContent=result[n];
-});
+  const btn = document.getElementById("button");
+
+  btn.addEventListener("click", () => {
+    const result = ["great", "good", "bad"];
+    const n = Math.floor(Math.random() * result.length);
+    btn.textContent=result[n];
+  });
+  
+})();
